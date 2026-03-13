e3c4b724ae97c1cb8557364560394b1ff927bc2c
jimmydani <danijimmy19@gmail.com>
2026-03-12 23:21:59 -0500

    Bug 1: Higher/lower hints are backward

--- logic_utils.py
+++ logic_utils.py
@@ -3,9 +3,9 @@ def get_range_for_difficulty(difficulty: str):
     if difficulty == "Easy":
         return 1, 20
     if difficulty == "Normal":
-        return 1, 100
-    if difficulty == "Hard":
         return 1, 50
+    if difficulty == "Hard":
+        return 1, 100
     return 1, 100
 
 
@@ -43,9 +43,9 @@ def check_guess(guess, secret):
 
     try:
         if guess > secret:
-            return "Too High", "📈 Go HIGHER!"
+            return "Too High", "📉 Go LOWER!"
         else:
-            return "Too Low", "📉 Go LOWER!"
+            return "Too Low", "📈 Go HIGHER!"
     except TypeError:
         g = str(guess)
         if g == secret:
